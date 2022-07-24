f552573c108fe6e82b19242454176b55ad1402c2
grigoriyMirakyan <grigoriy66660@gmail.com>
2022-07-24 17:36:29 +0300

    Task2

--- task1.py
+++ task1.py
@@ -4,25 +4,22 @@
 
 - при d = 3, π = 3.141'''
 
-import sys
 import math
 from decimal import *
-from math import factorial
 
 
-def chudnovsky(n):
+def bbp(n):
     pi = Decimal(0)
     k = 0
     while k < n:
-        pi += (Decimal(-1)**k)*(Decimal(factorial(6*k))/((factorial(k)**3)
-                                                         * (factorial(3*k))) * (13591409+545140134*k)/(640320**(3*k)))
+        pi += (Decimal(1)/(16**k))*((Decimal(4)/(8*k+1)) -
+                                    (Decimal(2)/(8*k+4))-(Decimal(1)/(8*k+5))-(Decimal(1)/(8*k+6)))
         k += 1
-    pi = pi * Decimal(10005).sqrt()/4270934400
-    pi = pi**(-1)
     return pi
 
 
 d = int(input("введите d:  "))
-getcontext().prec = d+1  #+1, потому что у нас всегда есть одна цифра перед запятой, поэтому нужно отдавать на одну цифру больше
-num_Pi = chudnovsky(d)
+# d+1, потому что у нас всегда есть одна цифра перед запятой, поэтому нужно отдавать на одну цифру больше
+getcontext().prec = d+1
+num_Pi = bbp(d)
 print('при d =', d, ', Pi =', num_Pi)
